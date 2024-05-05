96ce865b5726816116c521388fb8c3cc2ef75654
Elena Titenko <ellena.titenko@gmail.com>
2024-05-05 19:15:48 +0300

    check pushes

--- src/index.js
+++ src/index.js
@@ -23,33 +23,4 @@ root.render(
 
 serviceWorkerRegistration.register(); // Регистрация Service Worker'а
 
-const tokenString = document.getElementById("token");
-
-const firebaseApp = initializeApp({
-  apiKey: "AIzaSyBzoeeiWQ8Qu9XxJdc4V1fvu1RO7VDI4xs",
-  authDomain: "oxxawsm-dashboard.firebaseapp.com",
-  projectId: "oxxawsm-dashboard",
-  storageBucket: "oxxawsm-dashboard.appspot.com",
-  messagingSenderId: "809122524558",
-  appId: "1:809122524558:web:be31539cc4dd104e1308df",
-  measurementId: "G-1S40ERNJK7"
-});
-
-const messaging = getMessaging(firebaseApp);
-
-getToken(messaging)
-  .then((currentToken) => {
-    if (currentToken) {
-      tokenString.innerHTML = 'Token is ' + currentToken;
-    }
-  })
-  .catch(err => {
-    console.log('unable to get permission', err)
-  })
-
-onMessage(payload => {
-  console.log("Message received. ", payload);
-  const { title, ...options } = payload.notification;
-});
-
 reportWebVitals();
